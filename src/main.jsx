fae63df8cfbccf2bce339d2a74b115a1bf12bcc1
Marknejo Borja <marknejo246@gmail.com>
2025-02-19 10:31:35 -0500

    Refactoring Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Posts from "./routes/Posts";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayaout from "./routes/RootLayaout";
-import NewPost from "./Components/Newpost";
+import NewPost from "./routes/NewPost";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayaout />,
     children: [
-      { path: "/", element: <App /> }, //Dominio Main
-      { path: "/create-post", element: <NewPost /> },
+      {
+        path: "/",
+        element: <Posts />,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      }, //Dominio Main
     ],
   },
 ]);
